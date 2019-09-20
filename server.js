730c87c22177e1a3fc7f83628768330648541f53
dcarazo <dario_carazo@hotmail.com>
2019-09-20 18:11:17 -0300

    q

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const port = process.env.PORT || 3000;
 app.use(express.static(__dirname + '/public'));
 
 //Express HBS engine
-hbs.registerPartials(__dirname + '/Views/parciales')
+hbs.registerPartials(__dirname + '/views/parciales')
 app.set('view engine', 'hbs');
 
 app.get('/', (req, res) => {
