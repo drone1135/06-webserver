775aa998e8101462fbfd88c5bf256e38981b3c1c
dcarazo <dario_carazo@hotmail.com>
2019-09-23 13:27:47 -0300

    a

--- server.js
+++ server.js
@@ -9,24 +9,25 @@ const port = process.env.PORT || 3000;
 
 app.use(express.static(__dirname + '/public'));
 
-//Express HBS engine
-hbs.registerPartials(__dirname + '/views/parciales')
+// Express HBS engine
+hbs.registerPartials(__dirname + '/views/parciales');
 app.set('view engine', 'hbs');
 
 
 
 
-app.get('/views/home.hbs', (req, res) => {
+app.get('/', (req, res) => {
 
     res.render('home', {
-        nombre: 'dario carazo merelo',
+        nombre: 'fernando'
     });
-
 });
 
 app.get('/about', (req, res) => {
+
     res.render('about');
 });
+
 app.listen(port, () => {
     console.log(`Escuchando peticiones en el puerto ${ port }`);
 });
